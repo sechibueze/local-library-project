4f1e5a268fdaf0d52a07b7298423b9fa5f986cf6
Samuel Chibueze <sechibueze@gmail.com>
2023-01-24 14:57:56 +0100

    fix security key exposure

--- LibraryProject/settings.py
+++ LibraryProject/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa&0au6d#ru$rf5)sn(=gps5!+df22x=)w#t#h78$58vzn2vha'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
